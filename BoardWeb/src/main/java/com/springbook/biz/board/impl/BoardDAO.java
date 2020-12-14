515a9d8779a7182b5cb7b36a70ce9bd17a4b4e9f
khl6235 <khl6235@naver.com>
2020-12-15 01:21:05 +0900

    error page(Day4)

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -15,22 +15,24 @@ import com.springbook.biz.common.JDBCUtil;
 @Repository("boardDAO")
 public class BoardDAO {
 	
-	//JDBC ∞¸∑√ ∫Øºˆ
+	//JDBC Í¥ÄÎ†® Î≥ÄÏàò
 	private Connection conn = null;
 	private PreparedStatement stmt = null;
 	private ResultSet rs = null;
 	
-	//SQL ∏Ì∑…æÓµÈ
+	//SQL Î™ÖÎ†πÏñ¥Îì§
 	private final String BOARD_INSERT = "insert into board(seq, title, writer, content) values((select nvl(max(seq), 0)+1 from board), ?, ?, ?)";
 	private final String BOARD_UPDATE = "update board set title=?, content=?, where seq=?";
 	private final String BOARD_DELETE = "delete board where seq=?";
 	private final String BOARD_GET = "select * from board where seq=?";
 	private final String BOARD_LIST = "select * from board order by seq desc";
+	private final String BOARD_LIST_T = "select * from board where title like '%'||?||'%' order by seq desc";
+	private final String BOARD_LIST_C = "select * from board where content like '%'||?||'%' order by seq desc";
 	
-	//CRUD ±‚¥…¿« ∏ﬁº“µÂ ±∏«ˆ
-	//±€ µÓ∑œ
+	//CRUD Í∏∞Îä•Ïùò Î©îÏÜåÎìú Íµ¨ÌòÑ
+	//Í∏Ä ÏûëÏÑ±
 	public void insertBoard(BoardVO vo) {
-		System.out.println("===> JDBC∑Œ insertBoard() ±‚¥… √≥∏Æ");
+		System.out.println("===> JDBCÎ°ú insertBoard() Í∏∞Îä• Ï≤òÎ¶¨");
 		try {
 			conn = JDBCUtil.getConnection();
 			stmt = conn.prepareStatement(BOARD_INSERT);
@@ -45,9 +47,9 @@ public class BoardDAO {
 		}
 	}
 	
-	//±€ ºˆ¡§
+	//Í∏Ä ÏàòÏ†ï
 	public void updateBoard(BoardVO vo) {
-		System.out.println("===> JDBC∑Œ updateBoard() ±‚¥… √≥∏Æ");
+		System.out.println("===> JDBCÎ°ú updateBoard() Í∏∞Îä• Ï≤òÎ¶¨");
 		try {
 			conn = JDBCUtil.getConnection();
 			stmt = conn.prepareStatement(BOARD_UPDATE);
@@ -62,9 +64,9 @@ public class BoardDAO {
 		}
 	}
 	
-	//±€ ªË¡¶
+	//Í∏Ä ÏÇ≠Ï†ú
 	public void deleteBoard(BoardVO vo) {
-		System.out.println("===> JDBC∑Œ deleteBoard() ±‚¥… √≥∏Æ");
+		System.out.println("===> JDBCÎ°ú deleteBoard() Í∏∞Îä• Ï≤òÎ¶¨");
 		try {
 			conn = JDBCUtil.getConnection();
 			stmt = conn.prepareStatement(BOARD_DELETE);
@@ -77,9 +79,9 @@ public class BoardDAO {
 		}
 	}
 	
-	//±€ ªÛºº ¡∂»∏
+	//Í∏Ä ÏÉÅÏÑ∏ Ï°∞Ìöå
 	public BoardVO getBoard(BoardVO vo) {
-		System.out.println("===> JDBC∑Œ getBoard() ±‚¥… √≥∏Æ");
+		System.out.println("===> JDBCÎ°ú getBoard() Í∏∞Îä• Ï≤òÎ¶¨");
 		BoardVO board = null;
 		try {
 			conn = JDBCUtil.getConnection();
@@ -104,14 +106,21 @@ public class BoardDAO {
 		return board;
 	}
 	
-	//±€ ∏Ò∑œ ¡∂»∏
+	//Í∏Ä Î™©Î°ù Ï°∞Ìöå
 	public List<BoardVO> getBoardList(BoardVO vo){
-		System.out.println("===> JDBC∑Œ getBoardList() ±‚¥… √≥∏Æ");
+		System.out.println("===> JDBCÎ°ú getBoardList() Í∏∞Îä• Ï°∞Ìöå");
 		List<BoardVO> boardList = new ArrayList<BoardVO>();
 		try {
 			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(BOARD_LIST);
+			if(vo.getSearchCondition().equals("TITLE")) {
+				stmt = conn.prepareStatement(BOARD_LIST_T);
+			}
+			else if(vo.getSearchCondition().equals("CONTENT")) {
+				stmt = conn.prepareStatement(BOARD_LIST_C);
+			}
+			stmt.setString(1, vo.getSearchKeyword());;
 			rs = stmt.executeQuery();
+			
 			while(rs.next()) {
 				BoardVO board = new BoardVO();
 				board.setSeq(rs.getInt("SEQ"));
--- BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,5 +1,7 @@
 package com.springbook.view.board;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,12 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
-import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
 @SessionAttributes("board")
@@ -25,7 +26,14 @@ public class BoardController {
 	
 	//글 등록
 	@RequestMapping("/insertBoard.do")
-	public String insertBoard(BoardVO vo) {
+	public String insertBoard(BoardVO vo) throws IOException{
+		//파일 업로드 처리
+		MultipartFile uploadFile = vo.getUploadFile();
+		if(!uploadFile.isEmpty()) {
+			String fileName = uploadFile.getOriginalFilename();
+			uploadFile.transferTo(new File("D:/"+fileName));
+		}
+		
 		boardService.insertBoard(vo);
 		return "getBoardList.do";
 	}
@@ -63,6 +71,11 @@ public class BoardController {
 	//글 목록 검색
 	@RequestMapping("/getBoardList.do")
 	public String getBoardList(BoardVO vo, Model model) {
+		//Null Check
+		if(vo.getSearchCondition() == null)
+			vo.setSearchCondition("TITLE");
+		if(vo.getSearchKeyword() == null)
+			vo.setSearchKeyword("");
 		
 		//Model 정보 저장
 		model.addAttribute("boardList", boardService.getBoardList(vo));
