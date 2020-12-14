2d24b284c648d04dec1d31b282dfece114abc5e7
khl6235 <khl6235@naver.com>
2020-12-14 12:26:09 +0900

    board Spring MVC architecture(Day3)

--- BoardWeb/src/main/java/com/springbook/view/board/GetBoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/GetBoardController.java
@@ -4,14 +4,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
 
 public class GetBoardController implements Controller{
 	
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("글 상세 조회 처리");
 		
 		//1. 검색할 게시글 번호 추출
@@ -25,10 +27,11 @@ public class GetBoardController implements Controller{
 		BoardVO board = boardDAO.getBoard(vo);
 
 		//3. 검색 결과를 세션에 저장하고 상세 화면으로 이동
-		HttpSession session = request.getSession();
-		session.setAttribute("board", board);
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("board", board);
+		mav.setViewName("getBoard");
 		
-		return "getBoard";
+		return mav;
 	}
 
 }
