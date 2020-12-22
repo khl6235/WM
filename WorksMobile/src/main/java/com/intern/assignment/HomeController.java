54f50ed47333953cf194f9d10ca91b128524f47b
khl6235 <khl6235@naver.com>
2020-12-22 19:05:16 +0900

    mybatis mysql

--- WorksMobile/src/main/java/com/intern/assignment/HomeController.java
+++ WorksMobile/src/main/java/com/intern/assignment/HomeController.java
@@ -1,11 +1,17 @@
 package com.intern.assignment;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +24,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+    BasicDataSource dataSource;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -35,5 +44,39 @@ public class HomeController {
 		
 		return "home";
 	}
+	
+	@RequestMapping("/dbTest.do")
+    public String dbTest(Model model) {
+        Connection conn = null;
+        Statement stmt = null;
+        
+        try {
+            conn = dataSource.getConnection();
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select now() as now;");
+            
+            while(rs.next()) {
+                model.addAttribute("serverTime", rs.getString("now"));
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();    
+            
+        } finally {
+            try {
+                if(stmt != null) stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            
+            try {
+                if(conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }                        
+        }
+        
+        return "home";
+    }
 	
 }
